4e16233347f6c018441d68873bb2fa770fccef8a
Kay <kyv.hardeman@gmail.com>
2020-06-27 18:16:28 -0400

    Portfolio-working Navbar

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+//Main Component
+import React from 'react'
+import Navbar from "./Navbar"
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
